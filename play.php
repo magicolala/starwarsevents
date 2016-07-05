ad4fa3d1de05874f6d7ed46a35efdb3d527f02b8
Cédric Oloa <cedric.oloa@laposte.net>
2016-07-05 20:33:24 +0200

    creation du fichier play.php

---
+++ play.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/app/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$kernel->boot();
+
+$container = $kernel->getContainer();
+$container->enterScope('request');
+$container->set('request', $request);
+
+$templating = $container->get('templating');
+
+echo $templating->render(
+    'EventBundle:Default:index.html.twig', array('name'=>'Vador', 'count'=>3)
+);
