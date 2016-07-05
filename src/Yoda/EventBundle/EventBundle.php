3d075589f0eed392cf218e5d80f19489b41f7a03
Cédric Oloa <cedric.oloa@laposte.net>
2016-07-05 19:03:10 +0200

    creation du bundle EventBundle

--- src/Yoda/EventBundle/EventBundle.php
+++ src/Yoda/EventBundle/EventBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Yoda\EventBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class EventBundle extends Bundle
 {
 }
